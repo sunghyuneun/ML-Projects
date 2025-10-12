2864616f7968fa9f56a1883bd7fb427158284338
Greg Eun <33554699+GregEun@users.noreply.github.com>
2025-10-12 11:40:23 -0700

    Finished cartPole?

--- CartPole/cartPole.py
+++ CartPole/cartPole.py
@@ -1,21 +1,123 @@
 import gymnasium
+import numpy as np
+from collections import defaultdict
+import random
 
-env = gymnasium.make('CartPole-v1', render_mode="human")
+env = gymnasium.make('CartPole-v1')
 
 observation, info = env.reset(seed=42)
 terminated = False
 truncated = False
+
+#Position, Velocity, Angle, Angular Velocity Bins
+POS_BINS = np.linspace(-4.8,4.8,5)
+VEL_BINS = np.linspace(-3.5,3.5,5)
+ANG_BINS = np.linspace(-.418,.418,5)
+ANG_VEL_BINS = np.linspace(-5.0,5.0,5)
+
+#Initialize Q Table, Q_table[discrete_state,action] is Q(s,a)
+Q_table = defaultdict(lambda: np.zeros(env.action_space.n))
+
+#Epsilon and Decay rates, Epsilon is exploration rate
+epsilon = 1.0
+EPSILON_DECAY_RATE = 0.99995
+MIN_EPSILON = 0.01
+
+#Q-Learning Update, Alpha is Learning Rate, Gamma is Discount rate
+ALPHA = 0.1
+DISCOUNT_RATE = .99
+
+
+#print(f"Observation Space: {env.observation_space}")
+#print(f"Action Space (0=Left, 1=Right): {env.action_space}")
+
+MAX_EPISODES = 100000
+
+#Discretizes the state based on the (global) bins
+def discretize_state(state):
+    pos, vel, ang, ang_vel = state
+    
+    discrete_pos = np.digitize(pos,POS_BINS)
+    discrete_vel = np.digitize(vel, VEL_BINS)
+    discrete_ang = np.digitize(ang, ANG_BINS)
+    discrete_ang_vel = np.digitize(ang_vel, ANG_VEL_BINS)
+
+    return (discrete_pos, discrete_vel, discrete_ang, discrete_ang_vel)
+
+
+#Selects action based on epsilon
+def select_action(discrete_state, Q_table, epsilon):
+
+    #If below epsilon, do random
+    if random.random() < epsilon:
+        return env.action_space.sample()
+    
+    #Otherwise, return the action that provided maximum value at a state. 
+    else:
+        return np.argmax(Q_table[discrete_state])
+    
+def update_q_table(s, a, r, s_prime, done, Q_table):
+
+
+    #Finding the TD target
+    if done:
+        #If terminal state, no future state. Target is just r
+        max_future_q = 0
+    else:
+        # Q-Learning Off-Policy: Use max Q-Value of next state. 
+        max_future_q = np.max(Q_table[s_prime])
+
+    td_target = r + DISCOUNT_RATE * max_future_q
+
+    #TD Error:
+    td_error = td_target - Q_table[s][a]
+    
+    Q_table[s][a] += ALPHA * td_error
+
+
+
+#Training Episodes
+for episode in range(1, MAX_EPISODES):
+    first_state, _ = env.reset(seed=random.randint(0, 10000000) if episode == 1 else None)
+    current_discrete_state = discretize_state(first_state)
+    done = False
+
+    while not done:
+        action = select_action(current_discrete_state, Q_table, epsilon)
+
+        state, reward, terminated, truncated, info = env.step(action)
+        done = terminated or truncated
+
+        new_discrete_state = discretize_state(state)
+
+        old_q_value = Q_table[current_discrete_state][action]
+        update_q_table(current_discrete_state, action, reward, new_discrete_state, done, Q_table)
+
+        current_discrete_state = new_discrete_state
+        
+    epsilon = max(MIN_EPSILON, epsilon * EPSILON_DECAY_RATE)
+
+env.close()
+
+last_env = gymnasium.make('CartPole-v1',render_mode = 'human')
+
+first_state, _ = last_env.reset()
+current_discrete_state = discretize_state(first_state)
+done = False
 total_reward = 0
 
-print(f"Observation Space: {env.observation_space}")
-print(f"Action Space (0=Left, 1=Right): {env.action_space}")
+while not done:
+    action = select_action(current_discrete_state, Q_table, 0)
 
-while not terminated and not truncated:
-    action = env.action_space.sample()
+    state, reward, terminated, truncated, info = env.step(action)
+    done = terminated or truncated
 
-    observation, reward, terminated, truncated, info = env.step(action)
-    env.render()
+    new_discrete_state = discretize_state(state)
+
+    current_discrete_state = new_discrete_state
+    total_reward += reward
+    last_env.render()
 
 print(f"\nEpisode finished. Total Reward: {total_reward}")
 
-env.close()
+last_env.close()
