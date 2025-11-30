1ea2ae13fdb7d7dbf6822fc37ee4fa0d1f89f23d
CodeBlazing <gregoryeun@gmail.com>
2025-11-29 21:55:12 -0800

    Modified Stuff

--- HalfCheetah/main.py
+++ HalfCheetah/main.py
@@ -3,12 +3,15 @@ import gymnasium as gym
 env = gym.make("HalfCheetah-v5", render_mode="human")
 observation, info = env.reset()
 
+print(f"Observation Space: {env.observation_space}]\n Action Space: {env.action_space}")
+
+'''
 for _ in range(5000):
     action = env.action_space.sample()
     observation, reward, terminated, truncated, info = env.step(action)
 
     if terminated or truncated:
         observation, info = env.reset()
-
+'''
 
 env.close()
