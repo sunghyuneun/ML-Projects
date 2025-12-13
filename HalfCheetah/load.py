c8e73b803f8daacf16ae82d0bf3711cc80d94a72
Sung Hyun Eun <gregoryeun@gmail.com>
2025-12-12 21:24:15 -0800

    bro idk man atp

---
+++ HalfCheetah/load.py
@@ -0,0 +1,29 @@
+import gymnasium as gym
+from stable_baselines3 import PPO
+
+env = gym.make("HalfCheetah-v5", render_mode = "human")
+
+try:
+    model = PPO.load("halfcheetah_ppo_v5", env=env)
+except FileNotFoundError:
+    print("lol file is not here bro")
+    exit()
+
+print("model found.")
+
+observation, info = env.reset()
+episode_reward = 0
+
+for step in range(5000):
+    action, _ = model.predict(observation, deterministic = True)
+
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    episode_reward += reward
+
+    if terminated or truncated:
+        print(f"Episode finished after {step+1} steps. Reward: {episode_reward}")
+        observation, info = env.reset()
+        episode_reward = 0
+
+env.close()
--- HalfCheetah/main.py
+++ HalfCheetah/main.py
@@ -1,10 +1,13 @@
 import gymnasium as gym
 from stable_baselines3 import PPO
 
+LOG_DIR = "./ppo_halfcheetah_logs/"
+MODEL_PATH = "halfcheetah_ppo_baseline"
+
 env = gym.make("HalfCheetah-v5")
 observation, info = env.reset()
 
-model = PPO("MlpPolicy", env, verbose=1, device='cuda',n_steps=2048,batch_size=64,gamma=0.99)
+model = PPO("MlpPolicy", env, verbose=1, device='cuda',learning_rate = 3e-4, n_steps=2048,batch_size=64,gamma=0.99, tensorboard_log=LOG_DIR)
 
 print(f"Observation Space: {env.observation_space}]\n Action Space: {env.action_space}")
 
@@ -16,8 +19,8 @@ for _ in range(5000):
     if terminated or truncated:
         observation, info = env.reset()
 '''
-model.learn(total_timesteps=1_000_000)
+model.learn(total_timesteps=1_000_000, tb_log_name = "PPO1_RUN_1_Baseline")
 
-model.save("halfcheetah_ppo_v5")
+model.save(MODEL_PATH)
 
 env.close()
